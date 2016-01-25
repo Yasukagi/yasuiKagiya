0f9a47a786a6c04c103dc44999a6ba96f9896b7c
Niramiya Niraji <niraer0@gmail.com>
2016-01-25 21:33:44 +0900

    Remove unnecessary comment

--- src/main/java/testServlet.java
+++ src/main/java/testServlet.java
@@ -4,9 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Created by NIRAer on 2016/01/25.
- */
 public class testServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
