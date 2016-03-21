77f4c59a3fc1a904e43290303ff883b9934bcba8
Niramiya Niraji <niraer0@gmail.com>
2016-03-22 02:21:55 +0900

    Fix file and class name

--- src/main/java/com/yasukagi/dao/ISteamGameListDao.java
+++ src/main/java/com/yasukagi/dao/ISteamGameListDao.java
@@ -2,7 +2,7 @@ package com.yasukagi.dao;
 
 import java.util.List;
 
-public interface iSteamGameListDao {
+public interface ISteamGameListDao {
     List<SteamGameList> findAll();
     int insert(SteamGameList steamGameList);
 }
--- src/main/java/com/yasukagi/dao/SteamGameListDao.java
+++ src/main/java/com/yasukagi/dao/SteamGameListDao.java
@@ -4,7 +4,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SteamGameListDao implements iSteamGameListDao {
+public class SteamGameListDao implements ISteamGameListDao {
 
     private List<SteamGameList> convert(ResultSet rs) throws SQLException{
         List<SteamGameList> list = new ArrayList<>();
