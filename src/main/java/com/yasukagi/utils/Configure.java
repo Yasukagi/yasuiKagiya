c1f5b9cda9fde55bfbcd01f792b25825a1d63b05
Niramiya Niraji <niraer0@gmail.com>
2016-06-08 02:07:45 +0900

    Add configure class

---
+++ src/main/java/com/yasukagi/utils/Configure.java
@@ -0,0 +1,42 @@
+package com.yasukagi.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * プロパティファイルを読み込んで，値を取得するためのクラス
+ * 標準のライブラリだとインプットストリーム開いたり面倒な作業があるので，クラスにまとめた
+ */
+public class Configure {
+  private Properties properties;
+  private static final Logger logger = LogManager.getLogger();
+
+  /**
+   * 読み込むプロパティファイルを渡す
+   * @param file ファイル名
+   */
+  public Configure(String file) {
+    properties = new Properties();
+    try {
+      InputStream inputStream = new FileInputStream(file);
+      properties.load(inputStream);
+      inputStream.close();
+    } catch (IOException e) {
+      logger.info(e.getMessage());
+    }
+  }
+
+  /**
+   * プロパティのキーから値を取り出す
+   * @param key キー
+   * @return キーに対応する値
+   */
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
+}
